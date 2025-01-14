ba81f48ed4c07429bf005ed3e8c5c41fb9dee1d2
TheChuan1503 <2683390185@qq.com>
2025-01-14 12:37:46 +0800

    init

---
+++ mirror.js
@@ -0,0 +1,16 @@
+document.write(`
+<style>
+    .mirror-1{
+        background: #FFFF11;
+        opacity: 0.4;
+        width: 100%;
+        padding-top: 8px;
+        padding-bottom: 8px;
+        text-align: center;
+        font-size: 12px;
+    }
+</style>
+<div class="mirror-1">
+    本站为 <a href="http://2015xiaodian.yygzs.top">2015xiaodian.yygzs.top</a> 的镜像站
+</div>
+`)
